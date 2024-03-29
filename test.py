c927a6deb610cee0ae98192accbaf120f70a9600
kfirido <164353477+kfirido@users.noreply.github.com>
2024-03-29 15:57:32 +0000

    Copilot third commit

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 def sumNumbers(a,b):
     return a+b
+
+# Generate a random list of numbers with length 10
+# use constant for lenght and range of numbers
+import random
+LEN = 10
+MIN_VALUE = 0
+MAX_VALUE = 100
+numbers = [random.randint(MIN_VALUE, MAX_VALUE) for _ in range(LEN)]
